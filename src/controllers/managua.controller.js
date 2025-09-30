e84123d182c20fb56cda1326b138e14e414878c0
benchav <joshua44benja@gmail.com>
2025-09-29 21:15:06 -0600

    granada

--- src/controllers/managua.controller.js
+++ src/controllers/managua.controller.js
@@ -3,9 +3,9 @@ const service = require('../services/managua.service');
 
 async function getManagua(req, res, next) {
   try {
-    const { jinotepe, chontales, masaya } = await service.getAllByCity();
-    const total = (jinotepe?.length || 0) + (chontales?.length || 0) + (masaya?.length || 0);
-    res.json({ jinotepe, chontales, masaya, total });
+    const { jinotepe, chontales, masaya, granada } = await service.getAllByCity();
+    const total = (jinotepe?.length || 0) + (chontales?.length || 0) + (masaya?.length || 0) +(masaya?.length || 0);
+    res.json({ jinotepe, chontales, masaya, granada, total });
   } catch (err) {
     next(err);
   }
